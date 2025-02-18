27e2dacf748519f02f1ec83760ccc165bfca2735
sagyobake <sagyobake@localhost.localdomain>
2025-02-18 23:34:38 +0900

    upgrade

--- routes/index.tsx
+++ routes/index.tsx
@@ -55,10 +55,9 @@ const heartLayer = () => {
 }
 
 
-
 export default function () {
     return (
-        <html>
+        <html lang='ja'>
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
