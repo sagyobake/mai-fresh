c15a48dd0524734d3c6c1aa914656a5d0d4ed692
sagyobake <sagyobake@localhost.localdomain>
2025-02-18 16:05:48 +0900

    upgrade

---
+++ routes/_app.tsx
@@ -0,0 +1,6 @@
+import { type PageProps } from "$fresh/server.ts";
+export default function App({ Component }: PageProps) {
+    return (
+        <Component />
+    );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,43 @@
+export default function () {
+    return (
+        <html>
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Mai</title>
+                <link rel="stylesheet" href="/styles.css" />
+            </head>
+
+            <body>
+                <header>
+                    <h1>Mai</h1>
+                </header>
+
+                <main>
+
+                </main>
+
+                <footer>
+                    <div>
+                        <a class="twitter" href="https://x.com/4831338_">X (Twitter)</a>
+                    </div>
+
+                    <div>
+                        <a class='instagram'
+                            href="https://www.instagram.com/42251935maimai?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                            Instagram <i>42251935maimai</i>
+                        </a>
+                    </div>
+
+                    <div>
+                        <a class='instagram'
+                            href="https://www.instagram.com/boc_mai1024?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                            Instagram <i>boc_mai1024</i>
+                        </a>
+                    </div>
+                </footer>
+
+            </body>
+        </html>
+    );
+}
