38653d8f754cbb3c7dea4554ff7196e456715f31
sagyobake <sagyobake@localhost.localdomain>
2025-03-01 15:58:36 +0900

    upgrade

--- app.js
+++ app.js
@@ -8,13 +8,20 @@ Deno.serve(async (req) => {
             },
         });
     }
-    const { socket, response } = Deno.upgradeWebSocket(req);
+    const { socket, response } = Deno.upgradeWebSocket(req, 0);
     socket.addEventListener("open", () => {
         console.log("a client connected!");
     });
     socket.addEventListener("message", (event) => {
         if (event.data === "ping") {
             socket.send("pong");
+
+            const e = Math.E;
+            for (let x = -e; x <= e; x += 0.0001) {
+                const y = Math.sin(e ** 11 * x) * Math.sqrt((e ** 2 - x ** 2) / 1.8)
+                    + Math.log(Math.abs(x) + 0.7);
+                socket.send(JSON.stringify({ x, y }));
+            }
         }
     });
     return response;
