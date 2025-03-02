ecf700851b9874a08c179ee73f44b9783f9e0b60
sagyobake <sagyobake@localhost.localdomain>
2025-03-02 21:01:06 +0900

    upgrade

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 
-const socket = new WebSocket("ws://localhost:8000");
-//const socket = new WebSocket("wss://mai2.deno.dev");
+//const socket = new WebSocket("ws://localhost:8000");
+const socket = new WebSocket("wss://mai2.deno.dev");
 socket.addEventListener("open", (e) => {
     console.log(socket.readyState);
     socket.send('ping');
